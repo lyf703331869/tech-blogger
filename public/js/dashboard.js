2b44ed87aa609cb0b27c3fb0be3d6c5c99a0edad
lyf703331869 <yliu52@nyit.edu>
2022-06-24 00:07:30 -0400

    fix issue in dashboard button function

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -15,6 +15,7 @@ const delButtonHandler = async (event) => {
     }
   }
 };
+//when user click delete button in dashboard, fetch delete request
 document
   .querySelectorAll(".delete-btn")
   .forEach((elem) => elem.addEventListener("click", delButtonHandler));
@@ -24,6 +25,7 @@ const editButtonHandler = async (event) => {
   document.location.replace(`/dashboard/edit/${id}`);
 };
 
+//when user click on edit button, in dashboard, direct page to editing blog page
 document
   .querySelectorAll(".edit-btn")
   .forEach((elem) => elem.addEventListener("click", editButtonHandler));
@@ -31,5 +33,5 @@ document
 function addBlogHandler() {
   document.location.replace("/dashboard/new");
 }
-
+//when user click on add blog button, direct page to adding blog page.
 document.querySelector(".add-blog").addEventListener("click", addBlogHandler);
