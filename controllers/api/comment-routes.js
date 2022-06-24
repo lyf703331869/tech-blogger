529d180e577edc49a57b4f5bb55f3e83b369d5fb
lyf703331869 <yliu52@nyit.edu>
2022-06-23 23:52:59 -0400

    fix post founction for adding new comment

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const { Comment } = require("../../models");
 const withAuth = require("../../utils/auth");
-
+// post new comment to database.
 router.post("/", withAuth, async (req, res) => {
   try {
     const newComment = await Comment.create({
